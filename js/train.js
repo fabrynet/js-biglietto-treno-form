b05f016d3194166bd7d8b99fd48d0e28d11b14f3
Fabrizio <fabrynet.bianco@gmail.com>
2020-06-17 18:13:26 +0200

    Creazione biglietto su click makeTicket

--- js/train.js
+++ js/train.js
@@ -16,17 +16,47 @@ Categoria selezionata dall'utente
 
 var makeTicket = document.getElementById('makeTicket');
 var cancel = document.getElementById('cancel');
-var price;
-var trainNumber;
-var trainCarriage;
 
 makeTicket.addEventListener('click', function() {
+
   var name = document.getElementById('name').value;
   var surname = document.getElementById('surname').value;
   var km = document.getElementById('km').value;
   var category = document.getElementById('category').value;
+
+  var price;
+  var priceKm = 0.21;
+  var sale1 = 0.8;
+  var sale2 = 0.6;
+
+  if (category == "underage") {
+    price = km * priceKm * sale1;
+  } else if (category == "over65") {
+    price = km * priceKm * sale2;
+  } else {
+    price = km * priceKm;
+  }
+
+  var minTrainNumber = 10000;
+  var maxTrainNumber = 90000;
+  var minTrainCarriage = 1;
+  var maxTrainCarriage = 15;
+  var trainNumber = Math.floor(Math.random() * (maxTrainNumber - minTrainNumber + 1) + minTrainNumber);
+  var trainCarriage = Math.floor(Math.random() * (maxTrainCarriage - minTrainCarriage + 1) + minTrainCarriage);
+
+  var passenger = surname + " " + name;
+  document.getElementById('passenger').innerHTML = passenger;
+  document.getElementById('trainNumber').innerHTML = trainNumber;
+  document.getElementById('trainCarriage').innerHTML = trainCarriage;
+  document.getElementById('price').innerHTML = price;
+  document.getElementById('categoryTicket').innerHTML = category;
+
   console.log(name);
   console.log(surname);
   console.log(km);
   console.log(category);
+  console.log(price);
+  console.log(trainNumber);
+  console.log(trainCarriage);
+
 });
