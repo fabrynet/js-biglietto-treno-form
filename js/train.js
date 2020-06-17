4e2d85eefac2b50efde7f9677ec1e31bf72d6172
fabrynet <fabrynet.bianco@gmail.com>
2020-06-17 15:28:23 +0200

    Impostazione ex.

---
+++ js/train.js
@@ -0,0 +1,20 @@
+/*
+Il programma dovrà permettere l'inserimento del nome del passeggero, dei chilometri che vuole percorrere e la selezione di una categoria (over65, minorenne, maggiorenne).
+Sulla base di queste informazioni dovrà:
+calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+(Come esercizio precedente)
+Dovrà creare un finto biglietto del treno con:
+Nome passeggero
+Codice treno (numero casuale tra 90000 e 100000 escluso)
+Numero carrozza
+Prezzo calcolato
+Categoria selezionata dall'utente
+*/
+var nome;
+var km;
+var prezzo;
+var treno;
+var carrozza;
